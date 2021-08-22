6c5a9c8df5745df70bf92822e7d1da4532f69b44
AngelaJimenez <al.jimenezf@uniandes.edu.co>
2021-08-22 18:57:28 -0500

    ahora si

--- main.js
+++ main.js
@@ -13,6 +13,14 @@ module.exports ={
 http.createServer((req,res)=>{
     if(req.url ==="/api/proveedores")
     {
+        const url = "https://gist.githubusercontent.com/josejbocanegra/d3b26f97573a823a9d0df4ec68fef45f/raw/66440575649e007a9770bcd480badcbbc6a41ba7/proveedores.json";
+        axios.get(url).then((response) => {
+        fs.writeFile(
+          "./proveedores.json",JSON.stringify(response.data),"utf-8",
+          (err) => {
+            if (err) console.log(err);
+          }
+        );
         fs.readFile("proveedores.json",(err,data) =>{
             if(err){
                 console.log(err);
@@ -26,9 +34,21 @@ http.createServer((req,res)=>{
               const html = createHtmlProveedores(rta)
               res.write(html);
         });
+    });
     }
     if(req.url==="/api/clientes")
     {
+        const url =
+        "https://gist.githubusercontent.com/josejbocanegra/986182ce2dd3e6246adcf960f9cda061/raw/f013c156f37c34117c0d4ba9779b15d427fb8dcd/clientes.json";
+      axios.get(url).then((response) => {
+        fs.writeFile(
+          "./clientes.json",
+          JSON.stringify(response.data),
+          "utf-8",
+          (err) => {
+            if (err) console.log(err);
+          }
+        );
         fs.readFile("clientes.json",(err,data) =>{
             if(err){
                 console.log(err);
@@ -42,6 +62,7 @@ http.createServer((req,res)=>{
               const html = createHtmlCliente(rta)
               res.write(html);
         });
+    });
     }
 }).listen(8081);
 
